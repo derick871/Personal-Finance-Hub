fdbba74552abf787eb1bd26fc56b816101271e98
derick871 <wanyamaderrick4@gmail.com>
2026-05-17 14:22:21 +0300

    initialize a  repository

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FinanceContext from './Components/FinanceContext'
+function App() {
+
+  return (
+    <>
+    <FinanceContext/>
+      
+    </>
+  )
+}
+
+export default App
