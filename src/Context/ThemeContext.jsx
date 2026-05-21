c50ecb2b39e17612aa2097d943b9e9af1c57851c
derick871 <wanyamaderrick4@gmail.com>
2026-05-21 19:16:21 +0300

    apply useEffect to modify themecontext

--- src/Context/ThemeContext.jsx
+++ src/Context/ThemeContext.jsx
@@ -25,3 +25,26 @@ export function ThemeProvider({ children }) {
     }
     return 'light';
   });
+  useEffect(() => {
+    const root = window.document.documentElement;
+    
+    if (theme === 'dark') {
+      root.classList.add('dark');
+    } else {
+      root.classList.remove('dark');
+    }
+    
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  // Handler state togglers
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme, toggleTheme, isDark: theme === 'dark' }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
